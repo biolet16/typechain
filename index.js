0e942387c8563033fee7828f44aaa69363e9767f
biolet16 <violet2677@naver.com>
2021-05-05 20:52:05 +0900

    타입스크립트 연습1

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "bora", age = 27, gender = "woman";
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
+//# sourceMappingURL=index.js.map
